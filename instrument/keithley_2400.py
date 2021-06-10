6e6332eeebd3e5ddcc9639edc05c3712016be69e
vrcunha <vrcbr17@gmail.com>
2021-06-09 23:52:56 -0300

    Adding GUI, docstring and logging

--- instrument/keithley_2400.py
+++ instrument/keithley_2400.py
@@ -1,25 +1,38 @@
+import logging
+import logging.config
 import numpy as np
 import pyvisa as visa
 from pyvisa import errors
 
+logging.config.fileConfig('Keithley_log.ini')
+logger = logging.getLogger('root')
+
 rm = visa.ResourceManager('@py')
 
 def init_instrument(rm, port):
-    return rm.open_resource(port, baud_rate=9600,
-                            write_termination='\r', read_termination='\r')
+    """Function to configure and open the resource."""
+    try:
+        return rm.open_resource(port, baud_rate=9600,
+                                write_termination='\r', read_termination='\r')
+    except Exception as e:
+        logger.warning(e)
+
 def connect_instrument():
+    """Function to instanciate the instrument."""
     for instrument in rm.list_resources():
         try:
             inst = init_instrument(rm, instrument)
             inst.timeout = 5000
-        except visa.errors.VisaIOError:
-            print('Error to connect with Keithley.')
+        except visa.errors.VisaIOError as e:
+            logger.warning(e)
     return inst
 
 def get_id(inst):
-    print(inst.query('*IDN?')[:36])
+    """Function to get the instrument ID."""
+    return inst.query('*IDN?')[:36]
 
 def measure(inst, start, stop, step):
+    """Function that configures the measure."""
     result = ['volts, amps']
     for x in np.arange(start, stop, step):
         inst.write(f':SOUR:VOLT {x}')
@@ -27,61 +40,82 @@ def measure(inst, start, stop, step):
             query = inst.query(':READ?').split(',')
             result.append(f'{query[0]}, {query[1]}')
         except visa.errors.VisaIOError as e:
-            print(e)
+            logger.warning(e)
     return result
 
-def save_data(result):
-    filename = input('Enter the filename: ')
-    with open(f'{filename}.csv', 'w') as file:
-        for line in result:
-            file.write(f'{line}\n')
+def save_data(result, name):
+    """Function to save the measure in file."""
+    try:
+        with open('.'.join(name), 'w') as file:
+            for line in result:
+                file.write(f'{line}\n')
+    except Exception as e:
+        logger.warning(f'{e}')
+    return None
 
 def set_source_delay(inst, delay=0):
-    inst.write(f':SOUR:DEL {delay}')
+    """Function to set the delay."""
+    return inst.write(f':SOUR:DEL {delay}')
 
 def setting_measure(inst, source='VOLT', sensor='CURR'):
-    inst.write(f':SENS:FUNC "{source}", "{sensor}"')
+    """Function to set the source and the sense."""
+    return inst.write(f':SENS:FUNC "{source}", "{sensor}"')
 
 def turn_on(inst):
-    inst.write(':OUTP ON')
+    """Function to turn keithley on."""
+    return inst.write(':OUTP ON')
 
 def turn_off(inst):
-    inst.write(':OUTP OFF')
-
-def wich_panel(inst):
-    print(inst.query(':ROUT:TERM?'))
-
-def select_front_panel(inst):
-    inst.write(':ROUT:TERM FRONT')
-
-def select_rear_panel(inst):
-    inst.write(':ROUT:TERM REAR')
+    """Function to turn keithley off."""
+    return inst.write(':OUTP OFF')
 
 def set_compliance(inst, value=100e-3):
-    inst.write(f':SENS:CURR:PROT {value}')
+    """Function to set compliance."""
+    return inst.write(f':SENS:CURR:PROT {value}')
 
 def reset_instrument(inst):
-    inst.write('*RST')
+    """Function to reset instrument commands."""
+    return inst.write('*RST')
 
-def wire_mode(inst):
-    print(inst.query(':SYST:RSEN?'))
+def select_panel(inst, panel='FRONT'):
+    """Function to select panel."""
+    response = inst.query(':ROUT:TERM?')
+    if response == panel:
+        return None
+    elif response != panel:
+        if panel == 'FRONT':
+            return inst.write(':ROUT:TERM FRONT')
+        else:
+            return inst.write(':ROUT:TERM REAR')
 
-def two_wire_mode(inst):
-    inst.write(':SYST:RSEN OFF')
-
-def four_wire_mode(inst):
-    inst.write(':SYST:RSEN ON')
+def select_wire_mode(inst, mode='2Wire'):
+    response = inst.query(':SYST:RSEN?')
+    if response == '0' and mode=='2Wire':
+        return None
+    elif response == '1' and mode=='4Wire':
+        return None
+    elif response == '0' and mode=='4Wire':
+        return inst.write(':SYST:RSEN ON')
+    elif response == '1' and mode=='2Wire':
+        return inst.write(':SYST:RSEN OFF')
 
 def run_resistance_measure():
     pass
 
 def run_IV_measure(inst, start, stop, step, source='VOLT', sensor='CURR', compliance=100e-3, delay = 0):
-    get_id(inst)
+    """Function that run measure."""
+    logger.debug('Início da Medida')
     reset_instrument(inst)
+    select_panel(inst, panel='FRONT')
+    two_wire_mode(inst)
     setting_measure(inst, source, sensor)
     set_compliance(inst, compliance)
     set_source_delay(inst, delay=0)
     turn_on(inst)
     result = measure(inst, start, stop, step)
     turn_off(inst)
-    save_data(result)
+    logger.debug('Fim da Medida')
+
+if __name__ == 'main':
+    inst = connect_instrument()
+    print(get_id(inst))
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from instrument.keithley_2400 import connect_instrument, run_IV_measure
+from GUI.keithley_ui import Ui_MainWindow
 
-# TODO: colocar log no programa
-# TODO: começar interface gráfica com pyqt
-# TODO: fazer função para medição de resistencia com tensao cte
+# TODO: implementar test
+# TODO: botão abort medida
+# TODO: função para medição de resistencia com tensao cte
 
 settings_dict = {
     'start': -0.5,
@@ -14,6 +17,48 @@ settings_dict = {
     'delay': 0
 }
 
+class Keithley_MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Keithley_MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        self.pwr_on_btn.clicked.connect(self.power_on)
+        self.pwr_off_btn.clicked.connect(self.power_off)
+        self.conn_btn.clicked.connect(self.connection)
+        self.run_btn.clicked.connect(self.run)
+        self.save_btn.clicked.connect(self.save_file)
+        self.actionOpen.triggered.connect(self.open_file)
+        self.actionSave.triggered.connect(self.save_file)
+        self.actionSobre.triggered.connect(self.open_file)
+        self.actionExit.triggered.connect(self.close)
+
+    def power_on(self):
+        pass
+
+    def power_off(self):
+        pass
+
+    def connection(self):
+        connect_instrument()
+
+    def run(self):
+        pass
+
+    def save_file(self):
+        from instrument.keithley_2400 import save_data
+        name = QFileDialog.getSaveFileName(self, 'Save File',
+                                           '', 'csv')
+        # TODO: PEGAR O RESULTADO
+        save_data(list([1,'2\n',3,4]), name)
+
+    def open_file(self):
+        path = QFileDialog.getOpenFileName(self, 'Open a file', '',
+                                        'All Files (*.*)')
+        if path != ('', ''):
+            print("File path : "+ path[0])
+
 if __name__ == '__main__':
-    Keithley = connect_instrument()
-    run_IV_measure(Keithley, **settings_dict)
+    app = QApplication(sys.argv)
+    UI = Keithley_MainWindow()
+    UI.show()
+    sys.exit(app.exec_())
